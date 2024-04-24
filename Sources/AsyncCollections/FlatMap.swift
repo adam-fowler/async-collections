13e1fe31d961ce951395c8ccf7f3df3850e5ba5e
Mahdi Bahrami <github@mahdibm.com>
2024-04-24 14:31:29 +0330

    Add FlatMap functions (#4)

--- Sources/AsyncCollections/FlatMap.swift
+++ Sources/AsyncCollections/FlatMap.swift
@@ -1,7 +1,10 @@
 
 extension Sequence where Element: Sendable {
-    /// Returns an array containing the results of mapping the given async closure over
-    /// the sequence’s elements.
+    /// Returns an array containing the concatenated results of calling the
+    /// given transformation with each element of this sequence.
+    ///
+    /// Use this method to receive a single-level collection when your
+    /// transformation produces a sequence or collection for each element.
     ///
     /// The closure calls are made serially. The next call is only made once the previous call
     /// has finished. Returns once the closure has run on all the elements of the Sequence
@@ -10,28 +13,21 @@ extension Sequence where Element: Sendable {
     ///     element of this sequence as its parameter and returns a transformed value of
     ///     the same or of a different type.
     /// - Returns: An array containing the transformed elements of this sequence.
-    public func asyncMap<T>(_ transform: @Sendable (Element) async throws -> T) async rethrows -> [T] {
-        let initialCapacity = underestimatedCount
-        var result = ContiguousArray<T>()
-        result.reserveCapacity(initialCapacity)
-
-        var iterator = self.makeIterator()
-
-        // Add elements up to the initial capacity without checking for regrowth.
-        for _ in 0..<initialCapacity {
-            result.append(try await transform(iterator.next()!))
+    public func asyncFlatMap<T: Sequence>(_ transform: @Sendable (Element) async throws -> T) async rethrows -> [T.Element] {
+        var result: [T.Element] = []
+        for element in self {
+            result.append(contentsOf: try await transform(element))
         }
-        // Add remaining elements, if any.
-        while let element = iterator.next() {
-            result.append(try await transform(element))
-        }
-        return Array(result)
+        return result
     }
 
-    /// Returns an array containing the results of mapping the given async closure over
-    /// the sequence’s elements.
+    /// Returns an array containing the concatenated results of calling the
+    /// given transformation with each element of this sequence.
     ///
-    /// This differs from `asyncMap` in that it uses a `TaskGroup` to run the transform
+    /// Use this method to receive a single-level collection when your
+    /// transformation produces a sequence or collection for each element.
+    ///
+    /// This differs from `asyncFlatMap` in that it uses a `TaskGroup` to run the transform
     /// closure for all the elements of the Sequence. This allows all the transform closures
     /// to run concurrently instead of serially. Returns only when the closure has been run
     /// on all the elements of the Sequence.
@@ -41,7 +37,7 @@ extension Sequence where Element: Sendable {
     ///     element of this sequence as its parameter and returns a transformed value of
     ///     the same or of a different type.
     /// - Returns: An array containing the transformed elements of this sequence.
-    public func concurrentMap<T: Sendable>(priority: TaskPriority? = nil, _ transform: @Sendable @escaping (Element) async throws -> T) async rethrows -> [T] {
+    public func concurrentFlatMap<T: Sequence>(priority: TaskPriority? = nil, _ transform: @Sendable @escaping (Element) async throws -> T) async rethrows -> [T.Element] where T: Sendable {
         let result: ContiguousArray<(Int, T)> = try await withThrowingTaskGroup(of: (Int, T).self) { group in
             for (index, element) in self.enumerated() {
                 group.addTask(priority: priority) {
@@ -64,19 +60,23 @@ extension Sequence where Element: Sendable {
             }
             return result
         }
+
         // construct final array and fill in elements
         return [T](unsafeUninitializedCapacity: result.count) { buffer, count in
             for value in result {
                 (buffer.baseAddress! + value.0).initialize(to: value.1)
             }
             count = result.count
-        }
+        }.flatMap { $0 }
     }
 
-    /// Returns an array containing the results of mapping the given async closure over
-    /// the sequence’s elements.
+    /// Returns an array containing the concatenated results of calling the
+    /// given transformation with each element of this sequence.
     ///
-    /// This differs from `asyncMap` in that it uses a `TaskGroup` to run the transform
+    /// Use this method to receive a single-level collection when your
+    /// transformation produces a sequence or collection for each element.
+    ///
+    /// This differs from `asyncFlatMap` in that it uses a `TaskGroup` to run the transform
     /// closure for all the elements of the Sequence. This allows all the transform closures
     /// to run concurrently instead of serially. Returns only when the closure has been run
     /// on all the elements of the Sequence.
@@ -87,7 +87,7 @@ extension Sequence where Element: Sendable {
     ///     element of this sequence as its parameter and returns a transformed value of
     ///     the same or of a different type.
     /// - Returns: An array containing the transformed elements of this sequence.
-    public func concurrentMap<T: Sendable>(maxConcurrentTasks: Int, priority: TaskPriority? = nil, _ transform: @Sendable @escaping (Element) async throws -> T) async rethrows -> [T] {
+    public func concurrentFlatMap<T: Sequence>(maxConcurrentTasks: Int, priority: TaskPriority? = nil, _ transform: @Sendable @escaping (Element) async throws -> T) async rethrows -> [T.Element]  where T: Sendable {
         let result: ContiguousArray<(Int, T)> = try await withThrowingTaskGroup(of: (Int, T).self) { group in
             // Code for collating results copied from Sequence.map in Swift codebase
             let initialCapacity = underestimatedCount
@@ -112,12 +112,13 @@ extension Sequence where Element: Sendable {
             }
             return results
         }
+
         // construct final array and fill in elements
         return [T](unsafeUninitializedCapacity: result.count) { buffer, count in
             for value in result {
                 (buffer.baseAddress! + value.0).initialize(to: value.1)
             }
             count = result.count
-        }
+        }.flatMap { $0 }
     }
 }
